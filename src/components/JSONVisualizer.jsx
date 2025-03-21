aa53c63394143698715deebd593b93a47ec9c8ae
evalcopy <3valcopy@gmail.com>
2025-03-21 13:10:31 -0500

    JSONVisualizer added
    also still learning git commands

---
+++ src/components/JSONVisualizer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JSONVisualizer = () => {
+  return (
+    <div className="json-visualizer">
+      <h3>JSON Data Viewer</h3>
+      <p>We’ll map the file structure here.</p>
+    </div>
+  );
+};
+
+export default JSONVisualizer;
